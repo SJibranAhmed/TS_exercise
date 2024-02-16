08c38b83dc3e0639fefd3b55d803dba73067b6b9
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-16 21:37:39 +0500

    practice 1

---
+++ Personal_message/index.js
@@ -0,0 +1,2 @@
+var person_name = 'Sheikh Jibran Ahmed';
+console.log("Hello ".concat(person_name, "! \n your would like to learn Python today"));
---
+++ Personal_message/index.ts
@@ -0,0 +1,2 @@
+let person_name:string = 'Sheikh Jibran Ahmed'
+console.log(`Hello ${person_name}! \n your would like to learn Python today`)
