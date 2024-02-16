9ede26094518b6b37250ce6f92b35552b76531a7
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-16 23:42:43 +0500

    practice 10

---
+++ Names/nm.js
@@ -0,0 +1,6 @@
+// Sheikh Jibran Ahmed 16 / feb / 2024
+var friends_name = ['Jibran', 'Munaaf', 'Hasan', 'Talha'];
+console.log(friends_name[0]);
+console.log(friends_name[1]);
+console.log(friends_name[2]);
+console.log(friends_name[3]);
---
+++ Names/nm.ts
@@ -0,0 +1,7 @@
+// Sheikh Jibran Ahmed 16 / feb / 2024
+let friends_name = ['Jibran', 'Munaaf', 'Hasan', 'Talha']
+
+console.log(friends_name[0])
+console.log(friends_name[1])
+console.log(friends_name[2])
+console.log(friends_name[3])
