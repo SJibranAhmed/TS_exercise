ba8634969f2349a2111fd56f98e4178e732b3d85
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-17 00:09:28 +0500

    practice 11

---
+++ Greetings/greet.js
@@ -0,0 +1,5 @@
+var friends_name = ['Jibran', 'Hammad', 'Hassan', 'Sufyan'];
+console.log(friends_name[0] + 'student of governer house I.T program');
+console.log(friends_name[1] + 'So hard work man ');
+console.log(friends_name[2] + 'So lazy man');
+console.log(friends_name[3] + 'My elder brother');
---
+++ Greetings/greet.ts
@@ -0,0 +1,7 @@
+let friends_name : any = ['Jibran', 'Hammad', 'Hassan', 'Sufyan']
+
+console.log(friends_name[0] + 'student of governer house I.T program')
+console.log(friends_name[1] + 'So hard work man ')
+console.log(friends_name[2] + 'So lazy man')
+console.log(friends_name[3] + 'My elder brother')
+
