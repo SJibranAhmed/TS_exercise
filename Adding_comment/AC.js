05189c8413020e94708c3e49bf8bd27c578933c3
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-16 23:29:45 +0500

    practice 9

---
+++ Adding_comment/AC.js
@@ -0,0 +1,8 @@
+// Sheikh Jibran Ahmed 16 / feb / 2024
+//this program is reveal the Hadith..
+var Prophet_name = 'Muhammad ( P.B.U.A)';
+var message = Prophet_name + " said,\"\n\"Our Action depend upon our Intention";
+console.log(message);
+// var person_name = 'sir zaiullah khan';
+// console.log(person_name.toUpperCase());
+// console.log(person_name.toLowerCase());
---
+++ Adding_comment/AC.ts
@@ -0,0 +1,11 @@
+// Sheikh Jibran Ahmed 16 / feb / 2024
+
+//this program is reveal the Hadith..
+
+let Prophet_name : string = 'Muhammad ( P.B.U.A)'
+let message : string = `${Prophet_name} said,"\n"Our Action depend upon our Intention`
+console.log(message)
+
+// var person_name = 'sir zaiullah khan';
+// console.log(person_name.toUpperCase());
+// console.log(person_name.toLowerCase());
