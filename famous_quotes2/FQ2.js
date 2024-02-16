bbfef786cedee108f9025b56bbe6b5c0656f609f
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-16 22:25:04 +0500

    practice 4

---
+++ famous_quotes2/FQ2.js
@@ -0,0 +1,3 @@
+var Prophet_name = 'Muhammad ( P.B.U.A)';
+var message = Prophet_name + " said,\"\n\"Our Action depend upon our Intention";
+console.log(message);
---
+++ famous_quotes2/FQ2.ts
@@ -0,0 +1,3 @@
+let Prophet_name : string = 'Muhammad ( P.B.U.A)'
+let message : string = `${Prophet_name} said,"\n"Our Action depend upon our Intention`
+console.log(message)
