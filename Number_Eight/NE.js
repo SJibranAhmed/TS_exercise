08c93eeb7e29f3948b7edb8cd4f4e09c96730df0
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-16 22:57:31 +0500

    practice 6

---
+++ Number_Eight/NE.js
@@ -0,0 +1,6 @@
+var addition = 3 + 5;
+var subtraction = 24 - 16;
+var division = 64 / 8;
+var multiplication = 4 * 2;
+var result = "3 + 6 = " + addition + " \n \n24 - 16 = " + subtraction + " \n \n4 * 2 = " + multiplication + "\n \n64 / 7 = " + division;
+console.log(result);
---
+++ Number_Eight/NE.ts
@@ -0,0 +1,10 @@
+let addition :number = 3 + 5
+let subtraction : number = 24 - 16
+let division : number = 64 / 8
+let multiplication : number = 4 * 2
+let result : string = `3 + 6 = ${addition} \n 
+24 - 16 = ${subtraction} \n 
+4 * 2 = ${multiplication}\n 
+64 / 7 = ${division}`
+
+console.log(result)
