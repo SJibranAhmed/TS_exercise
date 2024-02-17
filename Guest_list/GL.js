27a8c1139ef687044dbc9102a782bc0ad7686e51
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-17 11:53:40 +0500

    practice 13

---
+++ Guest_list/GL.js
@@ -0,0 +1,4 @@
+var guest = ['Munaaf', 'Hasan', 'Talha'];
+for (var i = 0; i < guest.length; i++) {
+    console.log("Assalaamualaikum!\n \"" + guest[i] + "\" do you like to dinner with me tonight");
+}
---
+++ Guest_list/GL.ts
@@ -0,0 +1,5 @@
+let guest: any = ['Munaaf', 'Hasan', 'Talha']
+
+for(let i = 0; i < guest.length ; i++){
+    console.log(`Assalaamualaikum!\n "${guest[i]}" do you like to dinner with me tonight`)
+}
