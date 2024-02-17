c0d7c1b82ad04011b6d9b140ae94f868896458fe
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-17 23:16:17 +0500

    practice 23

---
+++ City_name/CN.js
@@ -0,0 +1,6 @@
+var city_name = function (city, country) {
+    return console.log("\"" + city + " , " + country + "\"");
+};
+city_name('Karachi', 'Pakistan');
+city_name('Tokyo', 'Japan');
+city_name('Mumbai', 'India');
---
+++ City_name/CN.ts
@@ -0,0 +1,6 @@
+let city_name = (city, country)=>{
+    return console.log(`"${city} , ${country}"`)
+}
+city_name('Karachi', 'Pakistan')
+city_name('Tokyo','Japan')
+city_name('Mumbai','India')
