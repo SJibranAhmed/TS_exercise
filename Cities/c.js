df95e4b504269bdfd9fa89893ca2c503faf54dca
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-17 23:05:00 +0500

    practice 22

---
+++ Cities/c.js
@@ -0,0 +1,8 @@
+var descrite_city = function (city, country) {
+    if (city === void 0) { city = 'Karachi'; }
+    if (country === void 0) { country = 'Pakistan'; }
+    return console.log(city + ' is the capital of ' + country);
+};
+descrite_city();
+descrite_city('Punjab');
+descrite_city('London', 'US');
---
+++ Cities/c.ts
@@ -0,0 +1,7 @@
+let descrite_city = (city='Karachi', country='Pakistan')=>{
+    return console.log(city + ' is the capital of ' + country)
+}
+
+descrite_city()
+descrite_city('Punjab')
+descrite_city('London', 'US')
