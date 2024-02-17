9fc97b72e9873f9e4f6a6f4a99c4ab625a957a72
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-17 19:28:21 +0500

    practice 16

---
+++ Favorite_fruit/FF.js
@@ -0,0 +1,13 @@
+var favorite_fruit = ['mango', 'banana', 'pineapple'];
+for (var _i = 0, favorite_fruit_1 = favorite_fruit; _i < favorite_fruit_1.length; _i++) {
+    var fruit = favorite_fruit_1[_i];
+    if (fruit == favorite_fruit[0]) {
+        console.log(fruit + ", is my favorite fruit");
+    }
+    else if (fruit == favorite_fruit[1]) {
+        console.log(fruit + ", is my favorite fruit");
+    }
+    else if (fruit == favorite_fruit[2]) {
+        console.log(fruit + ", is my favorite fruit");
+    }
+}
---
+++ Favorite_fruit/FF.ts
@@ -0,0 +1,11 @@
+let favorite_fruit : any =['mango', 'banana', 'pineapple']
+
+for (let fruit of favorite_fruit){
+    if(fruit == favorite_fruit[0] ){
+        console.log(`${fruit}, is my favorite fruit`)
+    }else if(fruit == favorite_fruit[1]){
+        console.log(`${fruit}, is my favorite fruit`)
+    }else if(fruit == favorite_fruit[2]){
+        console.log(`${fruit}, is my favorite fruit`)
+    }
+}
