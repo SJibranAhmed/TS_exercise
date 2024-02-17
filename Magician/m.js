8f883db5a6f95dbc4abc6c8bab15cd3f0eeccafc
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-17 23:34:10 +0500

    practice 24

---
+++ Magician/m.js
@@ -0,0 +1,8 @@
+var magicians_name = ['Jibran', 'Hasan', 'Munaaf'];
+var show_magician = function (arr) {
+    var array = arr;
+    for (var i = 0; i < array.length; i++) {
+        console.log(arr[i]);
+    }
+};
+show_magician(magicians_name);
---
+++ Magician/m.ts
@@ -0,0 +1,10 @@
+let magicians_name: string[] = ['Jibran', 'Hasan', 'Munaaf']
+
+let show_magician = (arr)=>{
+    let array = arr
+    for(let i = 0; i<array.length; i++){
+     console.log(arr[i])       
+}
+} 
+
+show_magician(magicians_name)
