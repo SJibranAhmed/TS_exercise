a71a62d13b5dfc558141a312ba2d06e844d0c4a9
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-17 22:11:51 +0500

    practice 19

---
+++ Pizzas/p.ts
@@ -0,0 +1,11 @@
+let pizzas : any = ['fajita', 'tikka', 'pepperoni']
+
+for (let i = 0; i < pizzas.length; i++ ){
+    if(pizzas[i] == 'fajita'){
+        console.log(`${pizzas[i]} pizza is so tasty..`)
+    }else if(pizzas[i] == 'tikka'){
+        console.log(`${pizzas[i]} pizza is so spicy..`)
+    }else{
+        console.log(`${pizzas[i]} pizza i don\'t eat `)
+    }
+}
