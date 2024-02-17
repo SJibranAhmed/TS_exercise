487a941553eea4d330ab039745e25e0110448dd4
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-17 22:41:20 +0500

    practice 21

---
+++ T-shirt/TS.js
@@ -0,0 +1,4 @@
+var make_shirt = function (size, message) {
+    return size + " meter is the size of shirt \n,\"" + message + "\" this message is print on it front";
+};
+console.log(make_shirt(10, 'Hell in Cell'));
---
+++ T-shirt/TS.ts
@@ -0,0 +1,5 @@
+let make_shirt = (size: number, message :string)=>{
+    return `${size} meter is the size of shirt \n,"${message}" this message is print on it front`
+}
+
+console.log(make_shirt(10,'Hell in Cell'))
