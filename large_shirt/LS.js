5b098a9438dfa85d2e95d944544813055353f966
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-17 22:54:39 +0500

    practice 21

---
+++ large_shirt/LS.js
@@ -0,0 +1,6 @@
+var make_shirt = function (size, message) {
+    return console.log(size + " size shirt with message '" + message + "'");
+};
+make_shirt('large', 'I love TypeScript');
+make_shirt('medium', 'I love TypeScript');
+make_shirt('any', 'different messages');
---
+++ large_shirt/LS.ts
@@ -0,0 +1,7 @@
+let make_shirt = (size : string, message : string)=>{
+ return console.log(`${size} size shirt with message '${message}'`)
+}
+
+make_shirt('large', 'I love TypeScript')
+make_shirt('medium', 'I love TypeScript')
+make_shirt('any', 'different messages')
