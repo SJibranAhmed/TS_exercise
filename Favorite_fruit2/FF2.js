c369efabc93509b409befc35102bd7344add20e9
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-21 21:58:29 +0500

    practice 29

---
+++ Favorite_fruit2/FF2.js
@@ -0,0 +1,12 @@
+var favorite_fruit = ['apple', 'mango', 'gauva'];
+for (var i = 0; i < favorite_fruit.length; i++) {
+    if (favorite_fruit[i] == 'apple') {
+        console.log("You really like " + favorite_fruit[i]);
+    }
+    else if (favorite_fruit[i] == 'mango') {
+        console.log("You really like " + favorite_fruit[i]);
+    }
+    else if (favorite_fruit[i] == 'gauva') {
+        console.log("You really like " + favorite_fruit[i]);
+    }
+}
---
+++ Favorite_fruit2/FF2.ts
@@ -0,0 +1,10 @@
+let favorite_fruit : string[] = ['apple', 'mango', 'gauva']
+for(let i = 0 ; i < favorite_fruit.length; i++){
+    if(favorite_fruit[i] == 'apple'){
+    console.log(`You really like ${favorite_fruit[i]}`)
+}else if(favorite_fruit[i] == 'mango'){
+    console.log(`You really like ${favorite_fruit[i]}`)
+}else if(favorite_fruit[i] == 'gauva'){
+    console.log(`You really like ${favorite_fruit[i]}`)
+}
+}
