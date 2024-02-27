dd7badbe822ebcc61bd3cb41e35282fcf489bd7e
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-27 06:09:42 +0500

    practice 35

---
+++ Intentional_error/IE.js
@@ -0,0 +1,9 @@
+var fruit = ['mango', 'gauva', 'grapes', 'orange', 'pappaya'];
+//here type the index value
+var index = fruit[3];
+if (index == undefined) {
+    console.log('index error');
+}
+else {
+    console.log("".concat(index, " is the fruit"));
+}
---
+++ Intentional_error/IE.ts
@@ -0,0 +1,10 @@
+let fruit:string[] = ['mango', 'gauva', 'grapes', 'orange', 'pappaya']
+
+//here type the index value
+let index = fruit[3]
+
+if(index == undefined){
+    console.log('index error')
+}else{
+    console.log(`${index} is the fruit`)
+}
