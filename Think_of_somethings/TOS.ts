52c7c071060b4ae4dba2ec637253d30a0534afb7
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-27 06:00:31 +0500

    practice 34

---
+++ Think_of_somethings/TOS.ts
@@ -0,0 +1,10 @@
+
+let user_data:Record<string, string|boolean|number> = {
+    name:'Jibran',
+    fname:'Abdul Raheem',
+    contact:923159936336,
+    wantToEngineer:true
+}
+
+console.log('printing user_data')
+console.log(user_data)
