9e83e312dc68e949ab627d76eacfebde5cce3c49
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-27 05:51:54 +0500

    practice 33

---
+++ Store_in_array/SIA.js
@@ -0,0 +1,3 @@
+var rivers_of_Pakistan = ['sindh', 'jhelum', 'satluj', 'ravi', 'channeb'];
+console.log('\n rivers of Pakistan');
+console.log(rivers_of_Pakistan);
---
+++ Store_in_array/SIA.ts
@@ -0,0 +1,4 @@
+let rivers_of_Pakistan:string[] = ['sindh', 'jhelum', 'satluj', 'ravi', 'channeb']
+
+console.log('\n rivers of Pakistan')
+console.log(rivers_of_Pakistan)
